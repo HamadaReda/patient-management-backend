c3fcc7f1ab686036ffdcf60dd9b7a835c4eacec7
Hamada Reda <enghamadareda444@gmail.com>
2026-01-08 16:31:31 +0200

    Working in patient-service --> Change project version to 3.25 and openApi docs for patient

--- patient-service/src/main/java/com/pm/patientservice/PatientServiceApplication.java
+++ patient-service/src/main/java/com/pm/patientservice/PatientServiceApplication.java
@@ -1,7 +1,17 @@
 package com.pm.patientservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Patient API",
+                version = "1.0",
+                description = "API documentation for managing patients"
+        )
+)
 
 @SpringBootApplication
 public class PatientServiceApplication {
--- patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -4,6 +4,8 @@ import com.pm.patientservice.common.ApiResponse;
 import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/patients")
+@Tag(name = "Patient", description = "API for managing Patients")
 public class PatientController {
 
     private final PatientService patientService;
@@ -25,6 +28,7 @@ public class PatientController {
     }
 
     @GetMapping
+    @Operation(summary = "Get Patients")
     public ResponseEntity<ApiResponse<List<PatientResponseDTO>>> getPatients() {
         List<PatientResponseDTO> patients = patientService.getPatients();
         ApiResponse<List<PatientResponseDTO>> response = ApiResponse.success(HttpStatus.OK.value(), "Patients fetched successfully", patients);
@@ -32,6 +36,7 @@ public class PatientController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get specific patient")
     public ResponseEntity<ApiResponse<PatientResponseDTO>> getPatient(@PathVariable UUID id){
         PatientResponseDTO patientResponseDTO = patientService.getPatient(id);
         ApiResponse<PatientResponseDTO> response = ApiResponse.success(
@@ -43,6 +48,7 @@ public class PatientController {
     }
 
     @PostMapping
+    @Operation(summary = "create a new patient")
     public ResponseEntity<ApiResponse<PatientResponseDTO>> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
         PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(patientResponseDTO.getId()).toUri();
@@ -51,6 +57,7 @@ public class PatientController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "update specific patient")
     public ResponseEntity<ApiResponse<PatientResponseDTO>> updatePatient(@PathVariable UUID id, @Valid @RequestBody PatientRequestDTO patientRequestDTO) {
         PatientResponseDTO patientResponseDTO = patientService.updatePatient(id, patientRequestDTO);
         ApiResponse<PatientResponseDTO> response = ApiResponse.success(
@@ -62,6 +69,7 @@ public class PatientController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "delete specific patient")
     public ResponseEntity<ApiResponse<PatientResponseDTO>> deletePatient(@PathVariable UUID id){
         PatientResponseDTO patientResponseDTO = patientService.deletePatient(id);
         ApiResponse<PatientResponseDTO> response = ApiResponse.success(
